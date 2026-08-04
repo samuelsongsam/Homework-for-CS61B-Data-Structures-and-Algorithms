530962631eb91016eef1b69d8a1062eb619259e2
samuelsongsam <songxintong0422@ruc.edu.cn>
2026-08-04 22:19:37 +0800

    update proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 public class TestArrayDequeGold {
     @Test
     public void testStudentArrayDeque() {
@@ -8,46 +9,35 @@ public class TestArrayDequeGold {
         String message = "";
 
         for (int i = 0; i < 1000; i++) {
+            int operation;
+
             if (solDeque.isEmpty()) {
-                Integer operation = StdRandom.uniform(2);
-                if (operation == 0) {
-                    Integer addFirstItem = StdRandom.uniform(100);
-                    stuDeque.addFirst(addFirstItem);
-                    solDeque.addFirst(addFirstItem);
-                } else {
-                    Integer addLastItem = StdRandom.uniform(100);
-                    stuDeque.addLast(addLastItem);
-                    solDeque.addLast(addLastItem);
-                }
+                operation = StdRandom.uniform(2);
             } else {
-                Integer operation = StdRandom.uniform(4);
-                switch (operation) {
-                    case 0:
-                        Integer addFirstItem = StdRandom.uniform(100);
-                        stuDeque.addFirst(addFirstItem);
-                        solDeque.addFirst(addFirstItem);
-                        message += "addFirst(" + addFirstItem + ")\n";
-                        break;
-                    case 1:
-                        Integer addLastItem = StdRandom.uniform(100);
-                        stuDeque.addLast(addLastItem);
-                        solDeque.addLast(addLastItem);
-                        message += "addFirst(" + addLastItem + ")\n";
-                        break;
-                    case 2:
-                        Integer stuRemoveFirst = stuDeque.removeFirst();
-                        Integer solRemoveFirst = solDeque.removeFirst();
-                        message += "removeFirst()\n";
-                        assertEquals(stuRemoveFirst, solRemoveFirst);
-                        break;
-                    default:
-                        Integer stuRemoveLast = stuDeque.removeLast();
-                        Integer solRemoveLast = solDeque.removeLast();
-                        message += "removeFirst()\n";
-                        assertEquals(message, solRemoveLast, stuRemoveLast);
-                }
+                operation = StdRandom.uniform(4);
+            }
+
+            if (operation == 0) {
+                Integer item = StdRandom.uniform(100);
+                stuDeque.addFirst(item);
+                solDeque.addFirst(item);
+                message += "addFirst(" + item + ")\n";
+            } else if (operation == 1) {
+                Integer item = StdRandom.uniform(100);
+                stuDeque.addLast(item);
+                solDeque.addLast(item);
+                message += "addLast(" + item + ")\n";
+            } else if (operation == 2) {
+                Integer expected = solDeque.removeFirst();
+                Integer actual = stuDeque.removeFirst();
+                message += "removeFirst()\n";
+                assertEquals(message, expected, actual);
+            } else {
+                Integer expected = solDeque.removeLast();
+                Integer actual = stuDeque.removeLast();
+                message += "removeLast()\n";
+                assertEquals(message, expected, actual);
             }
         }
-
     }
 }
